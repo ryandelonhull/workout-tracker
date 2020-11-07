a1e4efd1dad4c15110d7ada2b8537af3687fd64e
Ryan Hull <ryandelonhull@gmail.com>
2020-11-07 00:37:10 -0500

    add server.js file

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,35 +1,12 @@
-const router = require("express").Router();
-const Transaction = require("../models/exercise-plan.js");
-
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+const path = require("path");
+const router = require("express").Router()
+    // router.get("/", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../public/index.html"))
+    // });
+    router.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
     });
-});
-
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+    router.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
     });
-});
-
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-module.exports = router;
+    module.exports = router;
